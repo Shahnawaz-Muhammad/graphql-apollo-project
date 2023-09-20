0491657757e43df847cce970e78492c7e0196ff0
Shahnawaz-Muhammad <shahnawazbaltee55@gmail.com>
2023-09-20 18:10:15 +0500

    logo added to signup and login

--- client/src/pages/login/index.jsx
+++ client/src/pages/login/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import logo from "../../assets/images/random-click-logo.png";
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -25,14 +26,13 @@ const Login = () => {
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <a
           href="#"
-          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+          className="flex items-center mb-3 text-2xl font-semibold text-gray-900 dark:text-white"
         >
           <img
-            className="w-8 h-8 mr-2"
-            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+            className="w-full h-24 bg-contain"
+            src={logo}
             alt="logo"
           />
-          Flowbite
         </a>
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
